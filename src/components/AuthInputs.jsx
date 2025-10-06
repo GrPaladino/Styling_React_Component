914c6c159fb50ea6ca11bca03b920fde595de1a3
Pietro Salerno <EXTA6QI@posteitaliane.it>
2025-10-06 15:16:18 +0200

    Add tailwind

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,14 +1,8 @@
 import { useState } from 'react';
 import { styled } from 'styled-components';
 import Button from './Button';
-import InputComponent from './Input.jsx';
+import Input from './Input.jsx';
 
-const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -31,16 +25,17 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
-        <InputComponent type="email" label="Email" invalid={emailNotValid} onChange={(event) => handleInputChange('email', event.target.value)} />
-        <InputComponent type="password" label="Password" invalid={passwordNotValid} onChange={(event) => handleInputChange('password', event.target.value)} />
-      </ControlContainer>
-      <div className="actions">
-        <button type="button" className="text-button">
+    <div id="auth-inputs" 
+    className="w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800">
+      <div className="flex flex-col gap-2 mb-6">
+        <Input type="email" label="Email" invalid={emailNotValid} onChange={(event) => handleInputChange('email', event.target.value)} />
+        <Input type="password" label="Password" invalid={passwordNotValid} onChange={(event) => handleInputChange('password', event.target.value)} />
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
-        <Button className='button' onClick={handleLogin}>Sign In</Button>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
